b5ae92d627fe36a1664efcd14bffc799f4b7c335
Jeffrey Wilcke <stygeo@gmail.com>
2013-09-29 17:15:08 +0200

    Added gh pages helper

---
+++ bin/gh-pages.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+docco frame.js
+git commit -am "Updated annonated source"
+
+rm -rf /tmp/frame
+cp -R docs/ /tmp/frame
+rm /tmp/frame/frame.js
+cp frame.js /tmp/frame/frame.js
+git checkout gh-pages
+
+rm -rf *
+mv /tmp/frame/* .
+git commit -am "Updates pages."
+git push
