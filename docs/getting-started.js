b9db9f4a0b002541f637e40b28b82b9217a5d766
Jeffrey Wilcke <stygeo@gmail.com>
2013-09-30 20:43:30 +0200

    Updated example

--- docs/getting-started.js
+++ docs/getting-started.js
@@ -1,18 +1,55 @@
 $(function() {
-  var BookViewController;
+  var BookViewController, DateView;
+
+  DateView = Frame.View.extend({
+    constructor: function() {
+      Frame.View.call(this);
+
+      this.dateModel = new Frame.Model({date: new Date()});
+
+      this.dateModel.addObserverForKey(this, 'date');
+    },
+
+    draw: function() {
+      this.$.html(this.dateModel.date);
+
+      this.dateModel.date = new Date();
+    },
+
+    update: function() {
+      var self = this;
+
+      setTimeout(function() {
+        self.draw();
+      }, 1000);
+    },
+
+    observeValueForKey: function(key, value) {
+      this.update();
+    }
+  });
+  DateView.property('time')
+
   BookViewController = Frame.ViewController.extend({
     // If you want the root view (this.view) to latch to any existing element
     // set the 'el' attribute from within the initialize method. It will be passed on to the view.
     // If no element with the specified attributes exists it creates a new DOM node.
 
     // Initialize is called from the constructor of the controller.
-    initialize: function(options) {
+    constructor: function(options) {
+      Frame.ViewController.call(this, options);
+
       // Set up anything controller related. Views can be set up from within the viewDidLoad method.
     },
 
     // Set up your view and load any content that needs to be displayed on the view.
     viewDidLoad: function() {
-      console.log('viewDidLoad');
+      this.view.$.html('Hello world');
+
+      this.subview = new DateView();
+      this.view.addSubview(this.subview);
+
+      this.subview.update();
     }
   });
 
@@ -22,7 +59,7 @@ $(function() {
       // Method fired as soon as all javascript has been loaded and when the DOM is ready
 
       // Setup root view controller. All ViewControllers have a root view (controller.view) set.
-      this.rootViewController = new BookViewController;
+      this.rootViewController = new BookViewController();
 
       // Once this function returns, if a rootViewController is present, it will call loadView on the root view controller
     },
@@ -32,5 +69,4 @@ $(function() {
       console.log("willTerminate");
     }
   });
-
 });
