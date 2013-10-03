ed8b03d6c067f88850a74f8c9170ccf243b24774
Jeffrey Wilcke <stygeo@gmail.com>
2013-10-03 11:56:05 +0200

    Working todo example

--- docs/todo.js
+++ docs/todo.js
@@ -2,27 +2,86 @@ $(function() {
   // Define out store
   Frame.storeDefinition = [{storeName: 'todo'}];
 
-  var TodoItem = Frame.Model.extend(['title'], {
-    objectName: 'todo'
+  var TodoItem = Frame.Model.extend(['title'], {objectName: 'todo'});
+
+  var TodoItemView = Frame.View.extend({
+    element: 'li',
+    constructor: function(options) {
+      Frame.View.call(this, options);
+
+      this.todoItem = options.todoItem;
+    },
+
+    draw: function() {
+      this.$.html(this.todoItem.title);
+    },
   });
 
-  var store = new Frame.IndexedStore();
-  Frame.dataStore = store;
+  var NewTodoItemView = Frame.View.extend({
+    delegate: undefined,
 
-  // Open todo database
-  store.open('todo', function() {
+    events: {
+      'click button' : 'addTodoItem'
+    },
 
-    var todoItem = new TodoItem({title: "Todo item for " + new Date().getTime()});
+    draw: function() {
+      this.$.html("<input type='text' placeholder='What would you like todo'><button>Add Todo</button>");
+    },
 
-    todoItem.save([], {
-      success: function(a,b,c) {
-        console.log(a,b,c);
+    addTodoItem: function(ev) {
+      var todoItem = new TodoItem({title: this.$.find('input').val()});
 
-        store.getAllByStoreName('todo', function(data) {
-          console.log(data);
-        });
+      var $this = this;
+      todoItem.save([], {
+        success: function(a,b,c) {
+          console.log("Created new item");
+          $this.delegate.createdItem(todoItem);
+        }
+      });
+    },
+  });
 
-      }
-    });
+  var TodoViewController = Frame.ViewController.extend({
+    constructor: function(options) {
+      Frame.ViewController.call(this, options);
+
+      this.store = new Frame.IndexedStore();
+      Frame.dataStore = this.store;
+    },
+
+    viewDidLoad: function() {
+      var store = this.store;
+      var $this = this;
+
+      var newTodoItemView = new NewTodoItemView();
+      newTodoItemView.delegate = this;
+
+      this.view.addSubview(newTodoItemView);
+
+      store.open('todo', function() {
+        store.getAllByStoreName('todo', {
+          success: function(data) {
+            var todoItem = new TodoItem(data);
+            var todoItemView = new TodoItemView({todoItem: todoItem});
+
+            $this.view.addSubview(todoItemView);
+          }
+        })
+      })
+    },
+
+    // Called by NewTodoItemView as delegate
+    createdItem: function(todoItem) {
+      var todoItemView = new TodoItemView({todoItem: todoItem});
+
+      this.view.addSubview(todoItemView);
+    },
+  });
+
+  Frame.application = Frame.Application.extend({
+    didFinishLaunching: function() {
+      var todoController = new TodoViewController({el: "#todo"});
+      this.rootViewController = todoController;
+    }
   });
 });
