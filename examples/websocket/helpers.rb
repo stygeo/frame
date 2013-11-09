2b7dc003da950673bc480c1972c18a8240cf5169
Jeffrey Wilcke <stygeo@gmail.com>
2013-11-09 16:12:30 +0100

    Added some more sample stuff & faking some rails behaviour

--- examples/websocket/helpers.rb
+++ examples/websocket/helpers.rb
@@ -1,10 +1,3 @@
-module Type
-  DISCONNECT = 0
-  MESSAGE = 1
-  JSON = 2
-  EVENT = 3
-end
-
 def unpack_message payload
   data = JSON.parse(payload)
 end
@@ -23,7 +16,3 @@ def pack_message type, payload, extra=nil, channel=nil
   return {type: type, data: payload, channel: channel}.to_json
 end
 
-def generate_gid(length = 12)
-  o = [(0..9), ('a'..'z'), (0..9)].map { |i| i.to_a }.flatten
-  (0...length).map{ o[rand(o.length)] }.join
-end
--- examples/websocket/websockets.rb
+++ examples/websocket/websockets.rb
@@ -1,130 +1,58 @@
 require 'json'
-
 require 'sinatra/base'
-require 'faye/websocket'
 
-require './helpers'
+require './backend'
 
-class SocketBackend
-  KEEPALIVE_TIME = 15
-
-  class << self
-    attr_accessor :events
-
-    def setup
-      @events = {}
-
-      yield self
+# Fake rails
+module ActiveRecord
+  class Base
+    def save
+      # Persisting data to database from rails would go here
     end
 
-    def on(event, &block)
-      @events[event] ||= []
+    class << self
+      def find(id)
+        return self.new({id: 1, title: 'bar title', student_id: 1234})
+      end
+    end
+  end
+end
 
-      @events[event] << block
+# Faking rails model
+class Book < ActiveRecord::Base
+  include Frame::Model
+
+  attr_accessor :resource_name, :attributes
+
+  socket_sync proc { |book| "student-#{book.student_id}" }
+
+  def initialize(attributes = {})
+    self.resource_name = "book"
+    self.attributes = attributes
+
+    attributes.each do |key, value|
+      self.class.send(:attr_accessor, key) unless self.respond_to? key
+
+      self.instance_variable_set "@#{key}", value
     end
   end
 
-  def initialize(app)
-    @app = app
-    @clients = {}
-    @channels = {}
-
-    Faye::WebSocket.load_adapter('thin')
-  end
-
-  def call(env)
-    if Faye::WebSocket.websocket?(env)
-      req = Rack::Request.new(env)
-
-      ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME})
-
-      socket_id = req.path.split("/").last
-      puts socket_id
-
-      ws.on :open do |event|
-        message = pack_message(Type::EVENT, 'connect')
-
-        ws.send( message )
-
-        @clients[socket_id] = {socket: ws}
-      end
-
-      ws.on :message do |event|
-        data = unpack_message(event.data)
-        puts data
-
-        to_call = nil
-
-        case data["type"]
-        when Type::EVENT
-          event = data["data"]["name"].to_sym
-          if event == :subscribe
-            channel = (@channels[data["data"]["data"]] ||= {})
-            channel[socket_id] = @clients[socket_id]
-          else
-            if SocketBackend.events[event]
-              SocketBackend.events[event].each do |block|
-                block.call(@clients)
-              end
-            end
-          end
-        when Type::MESSAGE
-
-          if data["channel"] && @channels[data["channel"]]
-            to_call = proc { @channels[data["channel"]].each { |sock_id, client| client[:socket].send( pack_message(Type::MESSAGE, data["data"], nil, data["channel"]) ) } }
-          else
-            to_call = proc { @clients.each { |sock_id, client| client[:socket].send( pack_message(Type::MESSAGE, data["data"]) ) } }
-          end
-        end
-
-        if to_call
-          EM.next_tick { to_call.call }
-        end
-      end
-
-      ws.on :close do |event|
-        @clients.delete(socket_id)
-        @channels.each do |name, clients|
-          clients.delete(socket_id)
-        end
-      end
-
-      ws.rack_response
-    else
-      @app.call(env)
-    end
-  end
 end
 
 class App < Sinatra::Base
   set :sessions, true
   set :server, 'thin'
-  set :sockets, {}
-  set :channels, {}
 
   configure do
-    SocketBackend.setup do |socket|
-      socket.on :test_event do |clients|
-        data = { action: 'update', resource: 'books', data: {id: 1, title: 'bar title'} }
+    Frame::SocketBackend.setup do |socket|
+      socket.on :test_event do |group|
+        resource = Book.find(1)
 
-        clients.each {|sock_id, client| client[:socket].send( pack_message(Type::EVENT, 'resource:store:modification', data) ) }
+        resource.save
       end
     end
 
-    use SocketBackend
-  end
-
-  get '/' do
-    if !request.websocket?
-    else
-    end
-  end
-
-  # Initialize handshake
-  get "/websocket" do
-    id = generate_gid
-
-    {id: id, beat: 10}.to_json
+    use Frame::SocketBackend
   end
 end
 
